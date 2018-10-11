0254e5a5ce896b04fd47697591437fd9ef1f76ae
Katrin Sooäär <katrin.sooaar@khk.ee>
2018-10-11 11:30:58 +0300

    Lisatud teisendamine Vue abil

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Converter />
+  </div>
+</template>
+
+<script>
+import Converter from './components/Converter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Converter
+  }
+}
+</script>
+
+<style>
+</style>
